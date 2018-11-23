d191ff92e0fcbe577234c2d65610acc2ca1a1ce5
barbaradecares <barbaradecares@hotmail.com>
2018-11-23 10:11:17 -0600

    done

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,15 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-}
+  let index = 0
+  var keyboardEvent = document.body.addEventListener("keydown", function(event){
+    if (event.key == codes[index]) {
+      if (index == (codes.length) - 1) {
+        alert('Hurray!')
+      }
+      index++;
+    } else {
+      index = 0
+    }
+  });
+}	
