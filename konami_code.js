71ae279b4bcf7f2a8fcbb2ed057aa484c6593343
Alexander Carlin <ahcarlin@gmail.com>
2018-11-26 04:24:32 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+document.body.addEventListener('keydown', function(e) {
+  const keyName = e.key;
+  return keyName
+})
+
+let index = 0;
+
 function init() {
-  // your code here
+  document.addEventListener('keydown', function(e) {
+  const keyName = e.key;
+  if (keyName === codes[index]) {
+    index++;
+    if (index === codes.length) {
+      alert('Congratulations!')
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}, true)
 }
+
