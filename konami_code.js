6e799404912c3cb0ec95ab6fc55c257f9a21dccd
ch0mper <hannahchang17@gmail.com>
2018-11-26 01:25:14 -0600

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,32 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+
+    if (key === codes[index]) {
+      //console.log('key');
+      index++;
+
+      if (index === codes.length) {
+        //console.log('great!')
+        alert("achievement unlocked.");
+        index = 0;
+      }
+    } else {
+      index = 0;
+      //console.log('wrong');
+    }
+  })
 }
+
+// function init() {
+//   document.body.addEventListener('keydown', function(e) {
+//     console.log(e.key);
+//   })
+// }
+
+// init();
